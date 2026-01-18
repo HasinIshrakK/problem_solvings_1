8ecaf03032e548678b0975aa65f7f06e1d55cd00
HasinIshrakK <hasinishrakk@gmail.com>
2026-01-19 01:36:17 +0600

    Problem 8 solved

---
+++ P8.js
@@ -0,0 +1,15 @@
+// Reversing
+
+let x = 'hello world';
+let array = x.split(" ");
+let newArr = [];
+
+for (arr in array) {
+    const a = array[arr].charAt(0).toUpperCase() + array[arr].slice(1);
+    newArr.push(a);
+}
+// console.log(newArr);
+
+const str = newArr.join(" ");
+
+console.log(str)
